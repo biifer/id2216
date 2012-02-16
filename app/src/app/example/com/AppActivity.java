62d680f7eeae5c585f9659659a8b983ecfd5cdb1
Affe <affebaffe@hotmail.com>
2012-02-16 14:11:19 +0100

    test

--- app/src/app/example/com/AppActivity.java
+++ app/src/app/example/com/AppActivity.java
@@ -9,5 +9,6 @@ public class AppActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //HEJ HEJ
     }
 }
