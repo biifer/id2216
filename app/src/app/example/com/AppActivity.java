10f9bad2ea7069cab0f39ebeb69f9980bfdebd7a
Ivan <ivanp@kth.se>
2012-02-17 15:45:13 +0100

    Final merge and change of variable names complete

--- app/src/app/example/com/AppActivity.java
+++ app/src/app/example/com/AppActivity.java
@@ -7,43 +7,42 @@ import android.view.View;
 import android.widget.*;
 import android.view.View.OnClickListener;
 
+public class AppActivity extends Activity implements
+/** Called when the activity is first created. */
+AdapterView.OnItemSelectedListener {
+	String[] items = { "1km", "2km", "3km", "infinity" };
 
-public class AppActivity extends Activity implements 
-    /** Called when the activity is first created. */
-	AdapterView.OnItemSelectedListener {
-	String[] items = { "1km","2km","3km","infinity" };
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.normal);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.normal);
 
-Spinner spin = (Spinner) findViewById(R.id.spinner1);
+		Spinner spin = (Spinner) findViewById(R.id.spinner1);
 		spin.setOnItemSelectedListener(this);
 
-		ArrayAdapter<Object> aa = new ArrayAdapter<Object>(
-				this,
-				android.R.layout.simple_spinner_item, 
-				items);
+		ArrayAdapter<Object> aa = new ArrayAdapter<Object>(this,
+				android.R.layout.simple_spinner_item, items);
 
-		aa.setDropDownViewResource(
-		   android.R.layout.simple_spinner_dropdown_item);
+		aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spin.setAdapter(aa);
-        setContentView(R.layout.normal);
-         
-        Button generateMap = (Button)findViewById(R.id.button1);
-        generateMap.setOnClickListener(new OnClickListener() {
-			
+		setContentView(R.layout.normal);
+
+		Button generateMap = (Button) findViewById(R.id.generate_map_button);
+		generateMap.setOnClickListener(new OnClickListener() {
+
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent myIntent = new Intent(v.getContext(), PreviewActivity.class);
+				Intent myIntent = new Intent(v.getContext(),
+						PreviewActivity.class);
 				v.getContext().startActivity(myIntent);
 			}
 		});
-    }
+	}
+
 	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
 			long arg3) {
 	}
-	public void onNothingSelected(AdapterView<?> arg0) {		
+
+	public void onNothingSelected(AdapterView<?> arg0) {
 	}
 }
