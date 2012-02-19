0d2698a9fbfb6ec02b14c7bdab3dd492de6d3ab6
Affe <affebaffe@hotmail.com>
2012-02-19 13:43:32 +0100

    fixed some values and preview.xml

--- app/src/app/example/com/AppActivity.java
+++ app/src/app/example/com/AppActivity.java
@@ -4,26 +4,45 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.*;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class AppActivity extends Activity {
-	
+public class AppActivity extends Activity implements
+/** Called when the activity is first created. */
+AdapterView.OnItemSelectedListener {
+	String[] items = { "1km", "2km", "3km", "infinity" };
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.normal);
-         
-        Button generateMap = (Button)findViewById(R.id.generate_map_button);
-        generateMap.setOnClickListener(new OnClickListener() {
-			
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.normal);
+
+		Spinner spin = (Spinner) findViewById(R.id.spinner1);
+		spin.setOnItemSelectedListener(this);
+
+		ArrayAdapter<Object> aa = new ArrayAdapter<Object>(this,
+				android.R.layout.simple_spinner_item, items);
+
+		aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin.setAdapter(aa);
+		setContentView(R.layout.normal);
+
+		Button generateMap = (Button) findViewById(R.id.generate_map_button);
+		generateMap.setOnClickListener(new OnClickListener() {
+
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent myIntent = new Intent(v.getContext(), PreviewActivity.class);
+				Intent myIntent = new Intent(v.getContext(),
+						PreviewActivity.class);
 				v.getContext().startActivity(myIntent);
 			}
 		});
-    }
+	}
+
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+	}
+
+	public void onNothingSelected(AdapterView<?> arg0) {
+	}
 }
--- app/src/app/example/com/MainActivity.java
+++ app/src/app/example/com/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
-	//	setContentView(R.layout.main);
+		setContentView(R.layout.normal);
 
 		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
