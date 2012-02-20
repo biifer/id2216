37ea60cdb4ee06f93752bdbc0f0bdf1edd939b73
Ivan <ivanp@kth.se>
2012-02-20 11:43:59 +0100

    added OptionsActivity

---
+++ app/src/app/example/com/OptionsActivity.java
@@ -0,0 +1,16 @@
+package app.example.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OptionsActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.id.options);
+	}
+
+}
