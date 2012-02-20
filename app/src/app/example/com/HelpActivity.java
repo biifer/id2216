09c9504b49ea8956257c4109a75edb3bf456426a
Ivan <ivanp@kth.se>
2012-02-20 12:09:08 +0100

    added help.xml and HelpActivity, they did not make it before

---
+++ app/src/app/example/com/HelpActivity.java
@@ -0,0 +1,14 @@
+package app.example.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help);
+	}
+}
