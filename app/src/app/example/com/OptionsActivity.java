828cfd90d38d69ebe2e2b38d04091f702a8d913d
Andreas Lindfors <alindf@kth.se>
2012-02-21 15:29:22 +0100

    MyPrefs filen har fått sina funktioner... load och save i appactivity och previewactivity

--- app/src/app/example/com/OptionsActivity.java
+++ app/src/app/example/com/OptionsActivity.java
@@ -1,10 +1,57 @@
 package app.example.com;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class OptionsActivity extends Activity{
 
+    private static final String PREFS_NAME = "PrefsFile";	
+    SharedPreferences settings;
+    SharedPreferences.Editor editor;
+	
+	private void saveIntToMyPrefs(String key, int value) {
+		settings = getSharedPreferences(PREFS_NAME, 0);
+		editor = settings.edit();
+		editor.putInt(key, value);
+		editor.commit();
+	}
+	
+	private int loadIntFromMyPrefs(String key) {
+		int defValue = -1;
+		settings = getSharedPreferences(PREFS_NAME, 0);
+		int value = settings.getInt(key, defValue);
+		return value;
+	}
+	
+	private void saveStringToMyPrefs(String key, String value) {
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+	    SharedPreferences.Editor editor = settings.edit();
+	    editor.putString(key, value);
+	    editor.commit();
+	}
+	
+	private String loadStringFromMyPrefs(String key) {
+		String defValue = "-1";
+		settings = getSharedPreferences(PREFS_NAME, 0);
+		String value = settings.getString(key, defValue);
+		return value;	
+	}
+	
+	private void saveBooleanToMyPrefs(String key, Boolean value) {
+		settings = getSharedPreferences(PREFS_NAME, 0);
+		editor = settings.edit();
+		editor.putBoolean(key, value);
+		editor.commit();
+	}
+	
+	private boolean loadBooleanFromMyPrefs(String key) {
+		boolean defValue = false;
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		boolean value = settings.getBoolean(key, defValue);
+		return value;
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
