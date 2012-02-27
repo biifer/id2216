d9651d31795a9a595e73be1675ff5b8c52cc1cf7
Andreas Lindfors <alindf@kth.se>
2012-02-27 09:41:27 +0100

    hämtar info från prefsfile för att inserta i databasen

--- app/src/app/example/com/DataBaseActivity.java
+++ app/src/app/example/com/DataBaseActivity.java
@@ -1,13 +1,61 @@
 package app.example.com;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class DataBaseActivity extends Activity {
 	DataBaseAdapter db;
-    /** Called when the activity is first created. */
+    
+	public static final String PREFS_NAME = "PrefsFile";
+	SharedPreferences settings;
+	SharedPreferences.Editor editor;
+
+	private void saveIntToMyPrefs(String key, int value) {
+		settings = getSharedPreferences(PREFS_NAME, 0);
+		editor = settings.edit();
+		editor.putInt(key, value);
+		editor.commit();
+	}
+
+	private int loadIntFromMyPrefs(String key) {
+		int defValue = -1;
+		settings = getSharedPreferences(PREFS_NAME, 0);
+		int value = settings.getInt(key, defValue);
+		return value;
+	}
+
+	private void saveStringToMyPrefs(String key, String value) {
+		settings = getSharedPreferences(PREFS_NAME, 0);
+		editor = settings.edit();
+		editor.putString(key, value);
+		editor.commit();
+	}
+
+	private String loadStringFromMyPrefs(String key) {
+		String defValue = "-1";
+		settings = getSharedPreferences(PREFS_NAME, 0);
+		String value = settings.getString(key, defValue);
+		return value;
+	}
+
+	private void saveBooleanToMyPrefs(String key, Boolean value) {
+		settings = getSharedPreferences(PREFS_NAME, 0);
+		editor = settings.edit();
+		editor.putBoolean(key, value);
+		editor.commit();
+	}
+
+	private boolean loadBooleanFromMyPrefs(String key) {
+		boolean defValue = false;
+		settings = getSharedPreferences(PREFS_NAME, 0);
+		boolean value = settings.getBoolean(key, defValue);
+		return value;
+	}	
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,12 +94,21 @@ public class DataBaseActivity extends Activity {
     }
     private void insertRoute(){ 
     	//Här ska vi hämta infon från "PrefsFile" och slänga in den i databasen TODOTODOTODO
-        db.open();
+        
+    	String uploader = loadStringFromMyPrefs("uploader");
+    	String routeName = loadStringFromMyPrefs("routename");
+    	String distance = loadStringFromMyPrefs("distance");
+    	String radius = loadStringFromMyPrefs("radius");
+    	String flags = loadStringFromMyPrefs("flags");
+    	String totalTime = loadStringFromMyPrefs("totaltime");
+    	String averageSpeed = loadStringFromMyPrefs("averagespeed");
+    	
+    	db.open();
         long id;
         id = db.insertRoute(
-        		"ivanabbeaffe",
-        		"marathon",
-        		"43km", "sdf", "sddf", "dsfdsf", "dsfsdf");
+        		uploader,
+        		routeName,
+        		distance, radius, flags, totalTime, averageSpeed);
         id = db.insertRoute(
         		"kthkarin",
         		"jogging",
